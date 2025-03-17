47c1f846d42f11d99d44eabdc7c164ae7dd7b28e
Jason Holler <holler.7@gmail.com>
2025-03-17 10:49:21 -0400

    Adding some missing files and updating for the first controller.

---
+++ landon-hotel/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.4.3"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("org.jetbrains.gradle.docker") version "1.5.2"
+}
+
+group = "com.hollersolutions.lil"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	//implementation("com.h2database:h2")
+	implementation("org.postgresql:postgresql")
+	implementation("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testAnnotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
